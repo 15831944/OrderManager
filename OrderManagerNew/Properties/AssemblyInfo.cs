2f8d54363edfdce3544f0cbf3e978845ab652b37
劉浩銘 <howwmingnew@gmail.com>
2020-09-15 17:17:24 +0800

    UI 修改，下次修改: 軟體名稱、軟體路徑、Url...

--- OrderManagerNew/Properties/AssemblyInfo.cs
+++ OrderManagerNew/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // 組件的一般資訊是由下列的屬性集控制。
 // 變更這些屬性的值即可修改組件的相關
 // 資訊。
-[assembly: AssemblyTitle("Order Manager")]
+[assembly: AssemblyTitle("PrintIn Order")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Inteware")]
-[assembly: AssemblyProduct("Order Manager")]
+[assembly: AssemblyCompany("PrintIn3D")]
+[assembly: AssemblyProduct("PrintIn Order")]
 [assembly: AssemblyCopyright("Inteware ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
